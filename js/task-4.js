b099cd3a0dc6888ee301d3ae60244092add38d24
Taras0319 <143548783+Taras0319@users.noreply.github.com>
2024-02-06 22:00:47 +0200

    work7-7

--- js/task-4.js
+++ js/task-4.js
@@ -1,28 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
-const form = document.querySelector('.login-form');
+loginForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
-  const formData = new FormData(form);
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
 
-  const obj = {};
-
-  let isEmpty = false;
-
-  formData.forEach(function (value, key) {
-    if (value === '') {
-      alert('All form fields must be filled in');
-      isEmpty = true;
-    } else {
-      obj[key] = value.trim();
-    }
-  });
-
-  if (!isEmpty) {
-    console.log(obj);
-    form.reset();
+  const formData = {
+    email: email,
+    password: password,
   };
-}
 
-form.addEventListener('submit', handleSubmit);
+  console.log(formData);
+  form.reset();
+}
